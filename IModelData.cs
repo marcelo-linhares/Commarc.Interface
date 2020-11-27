1e89bcab963c831bd8fa706450e06875a8c76b92
Marcelo Linhares da Silva <mcello@hotmail.com>
2020-11-27 08:16:59 -0300

    Add files via upload

---
+++ IModelData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commarc.Interface
+{
+    public interface IModelData
+    {
+        #region "Metodos indicados como publicos"
+        bool Save();
+        bool Validate();
+        void AddFieldRequired(string NomeCampo);
+        #endregion
+
+        #region "Metodos indicados como privados"
+        bool Update();
+        bool Insert();
+        bool Delete();
+        void FillData(System.Data.DataTable oDt);
+        void FillNew();
+        #endregion
+
+        #region "Propriedades"
+        bool IsNew { get; set; }
+        bool IsDeleted { get; set; }
+        List<String> GetFieldsRequired { get; }
+        bool IsChanged { get; }
+        #endregion
+    }
+}
