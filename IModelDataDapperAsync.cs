b62658adbc63aa3c2790be435ea3c0d0bf7c6e33
Marcelo - Commarc <commarc@losinox.com.br>
2021-08-20 08:25:32 -0300

    New version include Async Dapper Class in the contract

---
+++ IModelDataDapperAsync.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/*
+ * Contract Class for any kind of ASYNC Data or Repository tier C# application
+ * Target tier: Data or Repository
+ * .Net Framework build: 4.7
+ */
+
+namespace Commarc.Interface.Data
+{
+    public interface IModelDataDapperAsync
+    {
+        #region "Public Methods"
+        Task<bool> Save();
+        bool Validate();
+        void AddFieldRequired(string NomeCampo);
+        #endregion
+
+        #region "Private Methods"
+        Task<bool> Update();
+        Task<bool> Insert();
+        Task<bool> Delete();
+        #endregion
+
+        #region "Properties"
+        bool IsNew { get; }
+
+        /*
+         * This property might be 'true' when it intend deleted the data object. This property isn't a operation itself. It will be readed by 'Save()' method.
+         */
+        bool IsDeleted { get; set; }
+        
+        List<String> GetFieldsRequired { get; }
+        
+        /*
+         * This property should be 'true' when happen any modification in any attribute of data object
+         */
+        bool IsChanged { get; }
+        #endregion
+    }
+}
